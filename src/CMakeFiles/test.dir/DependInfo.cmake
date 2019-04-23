74ad8bea87582f0e823f768a09432323c827f19d
Pieter Thysebaert <pieter.thysebaert@gmail.com>
2019-04-23 20:15:36 +0200

    Initial commit including glfw3, glad, stb_image, glm

---
+++ src/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pth/opengl/src/glad.c" "/home/pth/opengl/src/CMakeFiles/test.dir/glad.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pth/opengl/src/test.cc" "/home/pth/opengl/src/CMakeFiles/test.dir/test.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/test.o"
+  "CMakeFiles/test.dir/glad.o"
+  "test.pdb"
+  "test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
