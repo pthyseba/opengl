862c1d53b0b02fa7406fc5cec00713a9f6eb2b1a
Pieter Thysebaert <pieter.thysebaert@gmail.com>
2019-05-09 19:14:29 +0200

    [DEV] Added perspective projection, floor, animation

--- src/test.cc
+++ src/test.cc
@@ -16,16 +16,25 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "uniform mat4 view;\n"
+    "uniform mat4 projection;\n"
+    "out vec4 color;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   color = vec4(aPos, 1.0f);\n"
+    "   gl_Position = projection*view*vec4(aPos, 1.0f);\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec4 color;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+  "   /*FragColor = vec4(1.0f, abs(gl_FragCoord.z), 0.2f, 1.0f);\n*/"
+    "   FragColor = color;\n"
     "}\n\0";
+
+bool animate = true;
+bool spacebarReleased = true;
 
 int main()
 {
@@ -104,14 +113,18 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
+         0.5f,  0.5f, -0.5f,  // upright, top right
+         0.5f, -0.5f, -0.5f,  // bottom right
+        -0.5f, -0.5f, -0.5f,  // bottom left
+        -0.5f,  0.5f, -0.5f,  // top left
+	-0.5f, -0.5f,  0.5f,  // floor, front left
+         0.5f, -0.5f,  0.5f   // floor, front right
     };
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
+        1, 2, 3,  // second Triangle
+	5, 2, 1,  // floor first triangle
+        5, 2, 4   // floor second triangle
     };
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -129,6 +142,8 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("../resources/textures/dark-grungy-wall-texture-background.jpg", &width, &height, &nrChannels, 0);
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
@@ -142,25 +157,40 @@ int main()
 
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
+    
     // render loop
     // -----------
+    int frameCount = 0;
     while (!glfwWindowShouldClose(window))
     {
         // input
         // -----
         processInput(window);
-
+	if (animate)
+	{
+	  frameCount = (frameCount+1)%500;
+	}
         // render
         // ------
+        
+        float translate = -1.0+(float)((frameCount+1)%500)*(0.5/500.0);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // draw our first triangle
         glUseProgram(shaderProgram);
+        glm::mat4 view = glm::mat4(1.0f);
+	// note that we're translating the scene in the reverse direction of where we want to move
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, translate)); 
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(45.0f), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
+	int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+	int viewLoc = glGetUniformLocation(shaderProgram, "view");
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
         // glBindVertexArray(0); // no need to unbind it every time 
  
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -187,6 +217,17 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    // toggle animation through space bar - handle long press/repeats
+    int spacebarState = glfwGetKey(window, GLFW_KEY_SPACE);
+    if (spacebarState == GLFW_PRESS && spacebarReleased)
+    {
+      animate = !animate;
+      spacebarReleased = false;
+    }
+    else if (spacebarState == GLFW_RELEASE)
+    {
+      spacebarReleased = true;
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
